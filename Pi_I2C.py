a116f86bfbe30a5c037d5f852abec3ad391920db
Emi <emco8172@colorado.edu>
2021-03-15 17:44:36 -0600

    Test Pi code

--- Pi_I2C.py
+++ Pi_I2C.py
@@ -1,23 +1,25 @@
+#import libraries
 import smbus
 import time
+
+
+# I2C device 
 bus = smbus.SMBus(1)
 DEVICE_ADDRESS = 0x08
+
+# Functions
 def StringToBytes(val):
     value = [];
     for i in  val:
         value.append(ord(i))
-        return value
+#         print(value.append(ord(i)))
+    
+    return value
+    
+    
+    
 #Turning it on
-data = StringToBytes('ON')
+data = StringToBytes('OFF')
 print(data)
+# print(type(data))
 bus.write_i2c_block_data(DEVICE_ADDRESS,0x00,data)
-# bus.write_i2c_block_data(DEVICE_ADDRESS,0x00,data2)
-#bus.write_byte_data(DEVICE_ADDRESS,0,StringToBytes('O'))
-#bus.write_byte_data(DEVICE_ADDRESS,0,StringToBytes('N'))
-#Turning it off
-# elif i = 0:
-#     bus.write_byte_data(DEVICE_ADDRESS,0,'O')
-#     bus.write_byte_data(DEVICE_ADDRESS,0,'F')
-#     bus.write_byte_data(DEVICE_ADDRESS,0,'F')
-word = bus.read_byte_data(DEVICE_ADDRESS,0)
-print(word)
